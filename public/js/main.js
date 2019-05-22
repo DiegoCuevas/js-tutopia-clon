f2331435ba1e69b7987310966a2f76a41e9a485f
Diego Cuevas <DiegoCuevas@users.noreply.github.com>
2019-05-22 12:40:57 -0500

    Create channel if doesn't exist (#16)
    * Create channel if doesn't exist
    
    * Fixed indentation

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,27 @@
-console.log('🚀');
+console.log("🚀");
+
+const channels = [{ name: "general" }];
+const user = [
+  {
+    id: "123123123123",
+    username: "Papito Mi rey",
+    channel: "1558544848926",
+    message: "123123123123"
+  }
+];
+
+function createChannel(nameChannel) {
+  if (!compareName(nameChannel))
+    channels.push({ id: new Date().getTime(), name: nameChannel });
+  else console.log("The channel already exist!");
+}
+
+function compareName(value) {
+  return channels.find(channel => {
+    return channel.name == value;
+  });
+}
+
+createChannel("que pasa papu");
+createChannel("duke");
+console.log(channels);
