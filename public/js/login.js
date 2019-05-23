206cb024142b759f141af4bd454aa9a8ca83bb94
DeyviConde <iamdeyvi@gmail.com>
2019-05-22 21:22:25 -0500

    Feature/addLoginForm (#25)
    * Add login form
    
    * Change var to let
    
    * change var to const
    
    * Fixed changes

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const $form = document.getElementById('loginForm');
+
+function getrgb() {
+  const r = Math.floor(Math.random() * 256); // Random between 0-255
+  const g = Math.floor(Math.random() * 256); // Random between 0-255
+  const b = Math.floor(Math.random() * 256); // Random between 0-255
+  const rgb = `rgb(${r}, ${g}, ${b})`;
+  return rgb;
+}
+
+$form.addEventListener('submit', event => {
+  event.preventDefault();
+  const username = event.target.elements.username.value;
+  const localUser = {
+    usernameKey: username,
+    colorKey: getrgb()
+  };
+  localStorage.setItem('user', JSON.stringify(localUser));
+  window.location = 'channel.html';
+});
+
+if (localStorage.getItem('user')) {
+  window.location = 'channel.html';
+}
